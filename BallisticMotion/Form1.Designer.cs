48fedaa1ebfbd244af6a11da4c8474b1f8636a13
bosbobos <ok__@outlook.com>
2022-05-13 18:11:56 +0300

    Программа закончена

--- BallisticMotion/Form1.Designer.cs
+++ BallisticMotion/Form1.Designer.cs
@@ -34,6 +34,11 @@
             this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -41,22 +46,24 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.Size = new System.Drawing.Size(140, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Начальная скорость";
+            this.label1.Text = "Начальная скорость (м/с)";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(130, 9);
+            this.label2.Location = new System.Drawing.Point(158, 9);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.Size = new System.Drawing.Size(83, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Угол (в градусах)";
+            this.label2.Text = "Угол (градусы)";
             // 
             // zedGraphControl1
             // 
+            this.zedGraphControl1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.zedGraphControl1.Location = new System.Drawing.Point(15, 81);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.zedGraphControl1.Name = "zedGraphControl1";
             this.zedGraphControl1.ScrollGrace = 0D;
             this.zedGraphControl1.ScrollMaxX = 0D;
@@ -73,26 +80,88 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(15, 26);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(109, 20);
+            this.textBox1.Size = new System.Drawing.Size(137, 20);
             this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "50";
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(133, 26);
+            this.textBox2.Location = new System.Drawing.Point(161, 26);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(92, 20);
             this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "45";
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 388);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 41);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Начертить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(245, 388);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 41);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Масштаб графика";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(130, 388);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 41);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Стандартный масштаб";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label3.Location = new System.Drawing.Point(360, 381);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(236, 24);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Максимальная высота:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label4.Location = new System.Drawing.Point(360, 405);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(198, 24);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Дальность полёта:";
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.zedGraphControl1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "MainWindow";
             this.Text = "BallisticMotion";
             this.ResumeLayout(false);
@@ -107,6 +176,11 @@
         private ZedGraph.ZedGraphControl zedGraphControl1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- BallisticMotion/Form1.cs
+++ BallisticMotion/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using ZedGraph;
 
@@ -13,16 +7,125 @@ namespace BallisticMotion
 {
     public partial class MainWindow : Form
     {
+        private double maxY;
+        private double L;
         public MainWindow()
         {
             InitializeComponent();
+            SetupGraph();  
+        }
+
+        private void SetupGraph()
+        {
+            GraphPane pane = zedGraphControl1.GraphPane;
+            pane.CurveList.Clear();
+            pane.XAxis.Title.Text = "S";
+            pane.YAxis.Title.Text = "Y";
+            pane.Title.Text = "Траектория движения";
+            pane.XAxis.Scale.MinAuto = true;
+            pane.XAxis.Scale.Max = 300;
+            pane.YAxis.Scale.MinAuto = true;
+            pane.YAxis.Scale.Max = 100;
+
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
         }
 
         private void DrawGraph()
         {
             GraphPane pane = zedGraphControl1.GraphPane;
             pane.CurveList.Clear();
-            PointPairList list = new PointPairList();  
+
+            LineItem line = pane.AddCurve("Траектория", CalculatePoints(), Color.Blue, SymbolType.None);
+            zedGraphControl1.Invalidate();
+
+            label3.Text = "Максимальная высота:" + Math.Round(maxY,1);
+            label4.Text = "Дальность полёта:" + Math.Round(L,1);
+        }
+
+        private PointPairList CalculatePoints()
+        {
+            PointPairList list = new PointPairList();
+            int V;
+            double ang;
+            if (textBox1.Text != "")
+                V = Convert.ToInt32(textBox1.Text);
+            else
+                V = 50;
+            if (textBox2.Text != "")
+                ang = Convert.ToInt32(textBox2.Text);
+            else
+                ang = 45;
+            double g = 10;
+            double x, y;
+            ang = ang * Math.PI / 180;
+
+            double t = (2 * V * Math.Sin(ang)) / g;
+
+            L = V * V * Math.Sin(ang * 2) / g;
+            maxY = V * V * Math.Pow(Math.Sin(ang), 2) / (2 * g);
+
+            for (double i = 0; i < t; i+=0.001)
+            {
+                x = V * Math.Cos(ang) * i;
+                y = V*Math.Sin(ang)*i - (g*i*i/2);
+
+                list.Add(x, y);
+            }
+
+            return list;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char num = e.KeyChar;
+            if (!Char.IsDigit(num) && num != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char num = e.KeyChar;
+            if (!Char.IsDigit(num) && num != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "" || Convert.ToInt64(textBox2.Text) < 90)
+                DrawGraph();
+            else
+                MessageBox.Show("Угол должен быть меньше 90 градусов!","Внимание!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GraphPane pane = zedGraphControl1.GraphPane;
+            
+            pane.XAxis.Scale.MinAuto = true;
+            pane.XAxis.Scale.MaxAuto = true;
+            pane.YAxis.Scale.MinAuto = true;
+            pane.YAxis.Scale.MaxAuto = true;
+
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            GraphPane pane = zedGraphControl1.GraphPane;
+
+            pane.XAxis.Scale.MinAuto = true;
+            pane.XAxis.Scale.Max = 300;
+            pane.YAxis.Scale.MinAuto = true;
+            pane.YAxis.Scale.Max = 100;
+
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
         }
     }
 }
