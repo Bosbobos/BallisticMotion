004e3d3911ceb016a857e2de3387e3f17eab43f7
ok__ <ok__@DESKTOP-NQB40SO>
2022-05-13 14:59:20 +0300

    Добавьте файлы проекта.

---
+++ BallisticMotion/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace BallisticMotion
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawGraph()
+        {
+            GraphPane pane = zedGraphControl1.GraphPane;
+            pane.CurveList.Clear();
+            PointPairList list = new PointPairList();  
+        }
+    }
+}
